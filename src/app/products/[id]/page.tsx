63d684f10db3b9a79f8339a8996ef35a73c54c7f
Wilhelm Reyes <wilhelmeselpro@gmail.com>
2025-06-17 20:34:06 -0600

    cambio en src/app/products/[id]/page.tsx

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,14 +1,14 @@
-import react from 'react';
+'use client' // Necesario si usas hooks como useState, useEffect o useParams
 
-import { useParams } from 'react-router-dom';
+import { useParams } from 'next/navigation'
 
-function ProductPage() {
-  const { id } = useParams();
+export default function ProductPage() {
+  const { id } = useParams() // Aquí obtienes el parámetro dinámico
 
   return (
     <div>
       <h1>Product Page</h1>
       <p>Product ID: {id}</p>
     </div>
-  );
+  )
 }
