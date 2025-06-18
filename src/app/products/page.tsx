1a6857c6f246e942b463fcbba2f6747659d5ca63
Wilhelm Reyes <wilhelmeselpro@gmail.com>
2025-06-17 20:29:17 -0600

    instalado dependencia react-dom creado nueva pagina de pruebas en productos.

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <h1>Products Page</h1>
+      <p>This is the products page.</p>
+    </div>
+  );
+}
+
+export default ProductsPage;
